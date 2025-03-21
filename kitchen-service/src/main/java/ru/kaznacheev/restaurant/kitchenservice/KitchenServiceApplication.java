1fe605cb0be4c14c91257cdc65757e0e79ae3e7e
Kaznacheev Pavel <kaznacheev9@yandex.ru>
2025-03-21 20:53:59 +0300

    refactor: remove underscores from package names

--- kitchen-service/src/main/java/ru/kaznacheev/restaurant/kitchenservice/KitchenServiceApplication.java
+++ kitchen-service/src/main/java/ru/kaznacheev/restaurant/kitchenservice/KitchenServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.kaznacheev.restaurant.kitchen_service;
+package ru.kaznacheev.restaurant.kitchenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
