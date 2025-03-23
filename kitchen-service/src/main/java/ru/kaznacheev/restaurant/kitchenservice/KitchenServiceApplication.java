9b489e043ad64c54260644f21cf79870277ff7d9
Kaznacheev Pavel <kaznacheev9@yandex.ru>
2025-03-23 20:11:45 +0300

    docs: add javadoc for existing code

--- kitchen-service/src/main/java/ru/kaznacheev/restaurant/kitchenservice/KitchenServiceApplication.java
+++ kitchen-service/src/main/java/ru/kaznacheev/restaurant/kitchenservice/KitchenServiceApplication.java
@@ -3,9 +3,17 @@ package ru.kaznacheev.restaurant.kitchenservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Основной класс приложения kitchen-service.
+ */
 @SpringBootApplication
 public class KitchenServiceApplication {
 
+	/**
+	 * Точка входа для запуска Spring Boot приложения.
+	 *
+	 * @param args Аргументы командной строки, переданные при запуске приложения.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(KitchenServiceApplication.class, args);
 	}
