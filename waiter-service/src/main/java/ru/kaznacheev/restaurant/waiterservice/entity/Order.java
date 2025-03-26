689d2c3fa9c4b88545cb085f295da28fdbd28394
Kaznacheev Pavel <kaznacheev9@yandex.ru>
2025-03-26 16:47:59 +0300

    feat: add order handler in waiter-service

---
+++ waiter-service/src/main/java/ru/kaznacheev/restaurant/waiterservice/entity/Order.java
@@ -0,0 +1,20 @@
+package ru.kaznacheev.restaurant.waiterservice.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+/**
+ * Класс, представляющий сущность заказа.
+ */
+@Getter
+@Setter
+@Builder
+public class Order {
+    private Integer id;
+    private Map<String, Integer> dishes;
+    private String comment;
+    private OrderStatus status;
+}
---
+++ waiter-service/src/main/java/ru/kaznacheev/restaurant/waiterservice/exception/BaseException.java
@@ -0,0 +1,16 @@
+package ru.kaznacheev.restaurant.waiterservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Базовое исключение.
+ */
+@AllArgsConstructor
+@Getter
+public class BaseException extends RuntimeException{
+    private final String title;
+    private final HttpStatus status;
+    private final String detail;
+}
